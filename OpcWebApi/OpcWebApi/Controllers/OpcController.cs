0bedd337c24e541981b7a9388e339d2dd2c196f4
caiqian <zero530@126.com>
2024-04-18 14:27:51 +0800

    add opc controller

---
+++ OpcWebApi/OpcWebApi/Controllers/OpcController.cs
@@ -0,0 +1,30 @@
+﻿using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace OpcWebApi.Controllers
+{
+    public class OpcController : ApiController
+    {
+        // GET api/values
+        public HttpResponseMessage GetVal(string id)
+        {
+            var values = new string[] { "value1", "value2" };
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, values);
+            response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+            return response;
+        }
+
+        public HttpResponseMessage GetVals(string ids)
+        {
+            var values = new string[] { "value1", "value2" };
+
+            HttpResponseMessage response = Request.CreateResponse(HttpStatusCode.OK, values);
+            response.Content.Headers.ContentType = new System.Net.Http.Headers.MediaTypeHeaderValue("application/json");
+
+            return response;
+        }
+    }
+}
